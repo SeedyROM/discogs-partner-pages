33dafe210dd844c89ca654098fcd21c4b2d61814
Zack Kollar <rallokkcaz@gmail.com>
2018-12-17 06:23:12 -0800

    Initial page template laid out

--- partner-page-template.php
+++ partner-page-template.php
@@ -1,3 +1,13 @@
-<?php
+<?php include('sections/header.php'); ?>
 
-?>
+<main id="main" class="site-main" role="main">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php the_title(); ?></h1>  
+        <div class="entry">
+            <?php the_content(); ?>
+        </div><!-- entry -->
+<?php endwhile; ?>
+<?php endif; ?>
+</main>
+
+<?php include('sections/footer.php'); ?>
